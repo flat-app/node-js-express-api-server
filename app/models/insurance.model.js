25c96f415625e7a4774743560dc6aa706c9f399d
Jeff Chiu <jeffchiu2000@gmail.com>
2020-09-07 10:32:26 -0700

    include the name of the apiinside our MongoDB model

--- app/models/insurance.model.js
+++ app/models/insurance.model.js
@@ -1,7 +1,7 @@
 module.exports = mongoose => {
   var schema = mongoose.Schema(
     {
-      title: String,
+      name: String,
       deductible: Number,
       stop_loss: Number,
       oop_max: Number
